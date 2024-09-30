6ed030b6007d11f9dd0e6577c4d41c0ed1d537b9
Sudesh Kharel <sudeshkharel15@gmail.com>
2024-09-30 17:31:36 +0545

    implemented delete data

--- app.js
+++ app.js
@@ -1,67 +1,84 @@
-const express = require('express')// express package express vnni variable ma rakhya
-const { blogs } = require('./model/index')
-const app = express()//storing it in variable app '"yo 2 ta line jun program ma pani hunxa"
+const express = require('express'); // express package
+const { blogs } = require('./model/index');
+const app = express();
 
-//telling the nodejs to set viewengine to ejs
-app.set('view engine', 'ejs')
+// telling Node.js to set view engine to ejs
+app.set('view engine', 'ejs');
 
-//database connection
-require("./model/index")
+// database connection
+require('./model/index');
 
-//form bata data aairaxa parse gara or handle gar vaneko ho
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// parse data from form submissions
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Home route
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
 
+// All blogs route
+app.get('/blog', async (req, res) => {
+    try {
+        // Fetch all blogs from the database
+        const allBlogs = await blogs.findAll();
+        console.log(allBlogs);
 
+        // Render the 'blog.ejs' page and pass the blogs data
+        res.render('blog', { blogs: allBlogs });
+    } catch (error) {
+        console.error('Error fetching blogs:', error);
+        res.status(500).send('An error occurred while fetching the blogs.');
+    }
+});
 
-app.get("/", (req, res) => {
-    res.send("hello world")
-})
-//allblogs
-app.get("/blog", async (req, res) => {
+// Create blog page route
+app.get('/createBlog', (req, res) => {
+    res.render('createBlog.ejs');
+});
 
-
-    //table bata data nikalnu paryo
-    //blogs vanni table bata sabai data dey vaneko
-    const allBlogs = await blogs.findAll()
-    console.log(allBlogs)
-
-    res.render('blog', { blogs: allBlogs })
-})
-
-
-//createblog
-app.get("/createBlog", (req, res) => {
-    res.render("createBlog.ejs")
-})
-
-
-
-//createBlog Post
-app.post("/createBlog", async (req, res) => {
+// Create blog POST route
+app.post('/createBlog', async (req, res) => {
     try {
         // Extract data from the form
         const { title, subtitle, description } = req.body;
 
-        // Insert the data into the "blogs" table
+        // Insert the data into the 'blogs' table
         await blogs.create({
             title: title,
             subtitle: subtitle,
-            description: description
+            description: description,
         });
 
         // Redirect to the blog listing page after successful submission
-        res.redirect("/blog");
+        res.redirect('/blog');
     } catch (error) {
-        console.error("Error creating blog:", error);
-        res.status(500).send("An error occurred while creating the blog.");
+        console.error('Error creating blog:', error);
+        res.status(500).send('An error occurred while creating the blog.');
     }
 });
 
+// Delete blog route
+app.get('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
 
+        // Delete the blog with the specified ID
+        await blogs.destroy({
+            where: {
+                id: id,
+            },
+        });
 
+        // After deletion, redirect to the blog list
+        res.redirect('/blog');
+    } catch (error) {
+        console.error('Error deleting blog:', error);
+        res.status(500).send('An error occurred while deleting the blog.');
+    }
+});
+
+// Start the server
 app.listen(3000, () => {
-    console.log("nodejs project has started at port 3000")
-
-})
+    console.log('Node.js project has started at port 3000');
+});
